d47833b3a0ebdf747db434949e99a9755638a478
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-29 16:18:08 -0500

    Add index and show actions

--- app/controllers/home_ownerships_controller.rb
+++ app/controllers/home_ownerships_controller.rb
@@ -1,2 +1,9 @@
 class HomeOwnershipsController < ApplicationController
+  def index
+    @ownerships = HomeOwnership.all
+  end
+
+  def show
+    @ownership = HomeOwnership.find(params[:id])
+  end
 end
