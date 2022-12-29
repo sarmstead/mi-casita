9f374e16fcd2acf541fda7289a77e5b520452d6f
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-29 15:45:07 -0500

    Add new and create actions with form view

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,4 +6,24 @@ class HousesController < ApplicationController
   def show
     @house = House.find(params[:id])
   end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to @house
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name, :exterior_color, :trim_color)
+  end
 end
