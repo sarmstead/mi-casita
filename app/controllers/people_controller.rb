b203e654385f1de3d40e4d8e15b2c5d67baf7c62
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-28 20:08:59 -0500

    Remove comments

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,10 +12,6 @@ class PeopleController < ApplicationController
   end
 
   def create
-    # When we visit /people/new, the GET /articles/new request is mapped to the new action. The new action does not attempt to save @person. Therefore, validations are not checked, and there will be no error messages.
-
-    # When we submit the form, the POST /people request is mapped to the create action. The create action does attempt to save @person. Therefore, validations are checked. If any validation fails, @person will not be saved, and app/views/people/new.html.erb will be rendered with error messages.
-
     @person = Person.new(person_params)
     @person.username = validate_username(@person.username)
 
