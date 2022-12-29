f0a8f66fd608097fe65d665b16012646a1bc7379
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-28 20:06:35 -0500

    Remove unnecessary global model concern

--- app/models/concerns/starts_with_at.rb
+++
@@ -1,5 +0,0 @@
-class StartsWithAt < ActiveModel::Validator
-  def validate(record)
-    record.errors.add :username, 'should begin with the @ symbol!' unless record.username.start_with? '@'
-  end
-end
