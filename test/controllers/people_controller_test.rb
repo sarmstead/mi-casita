c9269c53caeab0570e2538537fcfe29e1a2f83e1
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-28 21:21:54 -0500

    Fix failing response assertion

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get people_index_url
+  test 'should get index' do
+    get people_url
     assert_response :success
   end
 end
