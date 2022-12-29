8904c28e98d3e1ca5511f76c57c96447ded5a77c
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-28 20:36:27 -0500

    Add specs for creating, saving, and destroying and Person

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save without first name' do
+    person = Person.new(first_name: '', last_name: 'Jirafa', username: '@four_foot_beast')
+    assert_not person.save
+  end
+
+  test 'should not save without last name' do
+    person = Person.new(first_name: 'Winklewire', last_name: '', username: '@four_foot_beast')
+    assert_not person.save
+  end
+
+  test 'should not save without username' do
+    person = Person.new(first_name: 'Winklewire', last_name: 'Jirafa', username: '')
+    assert_not person.save
+  end
+
+  test "username must contain'@' symbol" do
+    person = Person.new(first_name: 'Winklewire', last_name: 'Jirafa', username: 'four_foot_beast')
+    assert_not person.save
+  end
+
+  test 'should delete record with valid user' do
+    assert Person.destroy(people(:tina).id)
+  end
 end
