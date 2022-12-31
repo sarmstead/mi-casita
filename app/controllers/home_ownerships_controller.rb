c89377acb9a04979a5ee4c8f81fa5ed00795ffef
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-31 13:30:40 -0500

    Add new and create actions

--- app/controllers/home_ownerships_controller.rb
+++ app/controllers/home_ownerships_controller.rb
@@ -6,4 +6,24 @@ class HomeOwnershipsController < ApplicationController
   def show
     @ownership = HomeOwnership.find(params[:id])
   end
+
+  def new
+    @ownership = HomeOwnership.new
+  end
+
+  def create
+    @ownership = HomeOwnership.new(ownership_params)
+
+    if @ownership.save
+      redirect_to @ownership
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def ownership_params
+    params.require(:home_ownership).permit(:person_id, :house_id)
+  end
 end
