4a3cd2d636288d865d12262047802180198641ac
Sunjay Armstead <connect@sunjayarmstead.local>
2022-12-31 16:21:31 -0500

    Add validation for proper hex values

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,8 +1,15 @@
 class House < ApplicationRecord
   validates :name, presence: true
-  validates :exterior_color, presence: true
-  validates :trim_color, presence: true
+  validates :exterior_color, presence: true, length: { within: 4..7 }
+  validates :trim_color, presence: true, length: { within: 4..7 }
+  validate :valid_color_value?
 
   has_many :home_ownerships
   has_many :people, through: :home_ownerships, dependent: :destroy
+
+  def valid_color_value?
+    errors.add :exterior_color, 'should begin with the # symbol!' unless exterior_color.starts_with? '#'
+
+    errors.add :trim_color, 'should begin with the # symbol!' unless trim_color.starts_with? '#'
+  end
 end
