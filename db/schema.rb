d75aa2731621a16d4ad9e807802e84c03c25a29c
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-04 09:09:57 -0500

    Generate Window model. Add associations and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_175945) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_135349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,4 +39,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_29_175945) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "windows", force: :cascade do |t|
+    t.bigint "house_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["house_id"], name: "index_windows_on_house_id"
+  end
+
+  add_foreign_key "windows", "houses"
 end
