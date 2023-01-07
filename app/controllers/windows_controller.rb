b563ca2443a2fefa9be62dfbb228827012732d1d
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-07 11:49:33 -0500

    Add new and create actions, view, and tests

--- app/controllers/windows_controller.rb
+++ app/controllers/windows_controller.rb
@@ -6,4 +6,24 @@ class WindowsController < ApplicationController
   def show
     @window = Window.find(params[:id])
   end
+
+  def new
+    @window = Window.new
+  end
+
+  def create
+    @window = Window.new(window_params)
+
+    if @window.save
+      redirect_to @window.house
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def window_params
+    params.require(:window).permit(:house_id)
+  end
 end
--- test/controllers/windows_controller_test.rb
+++ test/controllers/windows_controller_test.rb
@@ -10,4 +10,19 @@ class WindowsControllerTest < ActionDispatch::IntegrationTest
     get window_url(windows(:front))
     assert_response :success
   end
+
+  test 'should create new window' do
+    assert_difference 'Window.count' do
+      post windows_url, params: {
+        window: {
+          house_id: houses(:bomba).id
+        }
+      }
+    end
+
+    # Window.count in the test db is determined by test/fixtures/windows.yml
+    new_window = Window.find('2')
+
+    assert_redirected_to house_path(new_window.house)
+  end
 end
