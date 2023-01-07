4559285fdab117a869b3161507564bc6fa8edae8
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-07 18:24:07 -0500

    Add window tests

--- test/models/window_test.rb
+++ test/models/window_test.rb
@@ -1,7 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class WindowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save with valid associated house' do
+    window = Window.new(house_id: houses(:bomba).id)
+    assert window.save
+  end
+
+  test 'should delete record with valid window' do
+    assert Window.destroy(windows(:front).id)
+  end
 end
