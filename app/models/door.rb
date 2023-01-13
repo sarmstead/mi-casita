1c0fd96568a5460de15e9d4c52a63a46d4ef9116
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-13 05:37:47 -0500

    Migrate doors and add associations

--- app/models/door.rb
+++ app/models/door.rb
@@ -1,2 +1,3 @@
 class Door < ApplicationRecord
+  belongs_to :house
 end
--- db/migrate/20230113101733_create_doors.rb
+++ db/migrate/20230113101733_create_doors.rb
@@ -1,6 +1,7 @@
 class CreateDoors < ActiveRecord::Migration[7.0]
   def change
     create_table :doors, id: :uuid do |t|
+      t.references :house, foreign_key: true, type: :uuid, null: false
 
       t.timestamps
     end
