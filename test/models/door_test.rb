a9889b5412a817ab32f8eebc760f934934732fbc
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-13 05:58:28 -0500

    Add door fixture. Test creating and destroy door record.

--- test/models/door_test.rb
+++ test/models/door_test.rb
@@ -1,7 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class DoorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save with valid associated house' do
+    door = Door.new(house: houses(:bomba))
+    assert door.save
+  end
+
+  test 'should delete record with valid door' do
+    assert Door.destroy(doors(:front).id)
+  end
 end
