7b4435d03cf888485a7537c45b1647c68a06a47c
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-16 18:50:14 -0500

    Place devise_for in seperate scope block

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  scope module: :workshop, path: :workshop do
+  scope '/workshop' do
     devise_for :people, controllers: { registrations: 'devise/people/registrations' }
+  end
+
+  scope module: :workshop, path: :workshop do
     resources :people, :houses, :home_ownerships, :windows, :doors
   end
 end
