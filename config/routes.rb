f043d540aaabf750c2a39e717f67d5d69cdc146d
Sunjay Armstead <connect@sunjayarmstead.local>
2023-01-16 17:12:10 -0500

    Add devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   scope '/workshop' do
-    devise_for :people
+    devise_for :people, controllers: { registrations: 'devise/people/registrations' }
     resources :people, :houses, :home_ownerships, :windows, :doors
   end
 end
